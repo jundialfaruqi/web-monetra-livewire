b2670ce10a58c79183bdb7168f3d7e8f0e8a5ed1
jundialfaruqi <jundialfaruqi@gmail.com>
2026-01-12 00:46:01 +0700

    app name footer now app_name

--- resources/views/components/dashboard/footer.blade.php
+++ resources/views/components/dashboard/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer footer-center p-4 bg-base-100 text-base-content border-t border-base-200 mt-auto">
     <aside>
         <p>Copyright © {{ date('Y') }} - All right reserved by <span
-                class="font-bold">{{ config('app.name') }}</span></p>
+                class="font-bold">{{ $appSetting->app_name ?? config('app.name') }}</span></p>
     </aside>
 </footer>
